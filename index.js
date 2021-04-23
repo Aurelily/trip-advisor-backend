6ca6c45f244c0fc13be10864c054dc77b06f1aff
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2021-04-23 18:14:07 +0200

    initial

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+const { json } = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+// CONNEXION A MAILGUN
+const API_KEY = process.env.MG_API_KEY;
+const DOMAIN = process.env.MG_DOMAIN;
+const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+
+//Route post : envoyer les données du formulaire
+app.post("/form", async (req, res) => {
+  try {
+    console.log("Entrée dans le route Form");
+
+    const data = {
+      from: `${req.fields.firstname} ${req.fields.lastname} <${req.fields.email}`,
+      to: "aurelie.preaud@gmail.com",
+      subject: `Formulaire contact par ${req.fields.email}`,
+      text: `${req.fields.message}`,
+    };
+
+    mailgun.messages().send(data, (error, body) => {
+      console.log(body);
+      console.log(error);
+    });
+
+    res.json({ message: "Données reçues, mail envoyé" });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "All routes" });
+});
+
+app.listen(3000, () => {
+  console.log("Server started 😎 !");
+});
